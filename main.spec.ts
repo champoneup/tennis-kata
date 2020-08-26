26a2f2e9b4537a60093b57f1f2d59b0279ac856d
watunyoo <watunyou.i@codium.co>
2020-08-26 14:02:03 +0700

    add logic test case tennis

--- main.spec.ts
+++ main.spec.ts
@@ -1,6 +1,36 @@
 //
 // Sample outputs
-//
+
+import { updateGameScore, decideGameWinner, printScore} from "./main";
+import 'mocha';
+import { expect } from 'chai';
+
+describe('Game is On Going', () => { 
+it('should win the game when the score is 0-0', () => {
+    const game = updateGameScore(0, 0);
+    const winner = decideGameWinner(game)
+    expect(winner).equal('On Going');
+});
+it('should win the game when the score is 3-3', () => {
+  const game = updateGameScore(3, 3);
+  const winner = decideGameWinner(game)
+  expect(winner).equal('On Going');
+});
+it('should win the game when the score is 4-4', () => {
+  const game = updateGameScore(4, 4);
+  const winner = decideGameWinner(game)
+  expect(winner).equal('On Going');
+});
+it('should win the game when the score is 5-4 or 4-5', () => {
+  const game = updateGameScore(5, 4);
+  const winner = decideGameWinner(game)
+  expect(winner).equal('On Going');
+
+  const game1 = updateGameScore(4, 5);
+  const winner1 = decideGameWinner(game1)
+  expect(winner1).equal('On Going');
+});
+})
 
 
 describe('Player 1', () => {
@@ -14,7 +44,7 @@ describe('Player 1', () => {
     const winner = decideGameWinner(game);
 
     // then
-    expect(winner).toEqual('Player 1');
+    expect(winner).equal('Player 1');
   });
 
   it('should win the game when the score is 5-3', () => {
@@ -27,7 +57,7 @@ describe('Player 1', () => {
     const winner = decideGameWinner(game);
 
     // then
-    expect(winner).toEqual('Player 1');
+    expect(winner).equal('Player 1');
   });
 });
 
@@ -42,7 +72,7 @@ describe('Player 2', () => {
     const winner = decideGameWinner(game);
 
     // then
-    expect(winner).toEqual('Player 2');
+    expect(winner).equal('Player 2');
   });
 
   it('should win the game when the score is 3-5', () => {
@@ -55,11 +85,34 @@ describe('Player 2', () => {
     const winner = decideGameWinner(game);
 
     // then
-    expect(winner).toEqual('Player 2');
+    expect(winner).equal('Player 2');
   });
 });
 
 describe('Game', () => {
+  it('should print thirty - thirty when the score is 0-0', () => {
+    // given
+    const player1Points = 0;
+    const player2Points = 0;
+
+    // when
+    const score = printScore([player1Points, player2Points]);
+
+    // then
+    expect(score).equal('love - love');
+  });
+
+  it('should print thirty - thirty when the score is 1-1', () => {
+    // given
+    const player1Points = 1;
+    const player2Points = 1;
+
+    // when
+    const score = printScore([player1Points, player2Points]);
+
+    // then
+    expect(score).equal('fifteen - fifteen');
+  });
   it('should print thirty - thirty when the score is 2-2', () => {
     // given
     const player1Points = 2;
@@ -69,7 +122,7 @@ describe('Game', () => {
     const score = printScore([player1Points, player2Points]);
 
     // then
-    expect(score).toEqual('thirty - thirty');
+    expect(score).equal('thirty - thirty');
   });
 
   it('should print deuce when the score is 3-3', () => {
@@ -81,7 +134,7 @@ describe('Game', () => {
     const score = printScore([player1Points, player2Points]);
 
     // then
-    expect(score).toEqual('deuce');
+    expect(score).equal('deuce');
   });
 
   it('should print advantage player 1 when the score is 4-3', () => {
@@ -93,7 +146,7 @@ describe('Game', () => {
     const score = printScore([player1Points, player2Points]);
 
     // then
-    expect(score).toEqual('advantage player 1');
+    expect(score).equal('advantage player 1');
   });
 
   it('should print advantage player 2 when the score is 5-6', () => {
@@ -105,6 +158,6 @@ describe('Game', () => {
     const score = printScore([player1Points, player2Points]);
 
     // then
-    expect(score).toEqual('advantage player 2');
+    expect(score).equal('advantage player 2');
   });
 });
--- main.ts
+++ main.ts
@@ -1,3 +1,42 @@
 //
 // Your implementations
 // 
+class Game {
+    constructor(
+        public player1: number,
+        public player2: number
+    ){}
+
+    get winner(): string {
+        if ((this.player1 - this.player2) >= 2 ) {return 'Player 1'}
+        if ((this.player2 - this.player1) >= 2 ) {return 'Player 2'}
+        if (this.player1 <= 5 && this.player2 <= 5) {return 'On Going'}
+        return this.player1 > this.player2 ? 'Player 1': 'Player 2';
+    }
+}
+
+export function updateGameScore(player1: number, player2: number): Game{
+    return new Game(player1, player2);
+}
+
+export function decideGameWinner(game: Game):string {
+    return game.winner;
+}
+
+export function printScore(score: number[]):string {
+    const scoreDetail = ['love', 'fifteen', 'thirty','forty']
+    if (score[0] >= 3 && score[1] >= 3){
+        if(score[0] === score[1]){
+            return 'deuce'
+        }
+        else if(score[0] - score[1] == 1 || score[0] - score[1] == -1){
+            return score[0] > score[1] ? 'advantage player 1': 'advantage player 2'; 
+        }
+    } else {
+        if (score[0] !== score[1]){
+            return scoreDetail[score[0]] + ' - ' + scoreDetail[score[1]]
+        } else {
+            return scoreDetail[score[0]] + ' - ' + scoreDetail[score[1]]
+        }
+    }
+}
